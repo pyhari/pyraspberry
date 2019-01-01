05a23297213aadf1d9c104e0fca99c7d4a2c7254
Hari <rharikumar17@gmail.com>
2019-01-01 15:56:15 -0800

    Formatted code

--- fibonacci_with_recursion_memoization.py
+++ fibonacci_with_recursion_memoization.py
@@ -1,25 +1,24 @@
 import time
-#Fibonacci series with recursion
-memo={}
+import sys
+# Fibonacci series with recursion
+memo = {}
+sys.setrecursionlimit(5000)
+
+
 def fib_recursion(n):
-    
-    if(n<2):
-       return(1)
+    if n < 2:
+        return 1
     else:
         if n in memo:
             return memo[n]
         else:
-            fib= fib_recursion(n-1) + fib_recursion(n-2)
-            memo[n]=fib
-        return(fib)
-    
+            fib = fib_recursion(n-1) + fib_recursion(n-2)
+            memo[n] = fib
 
 
 start = time.time()
-input_length=1500
-print("Result is", fib_recursion(input_length))
+input_length = 1500
+print(type(input_length))
+print('Result is', fib_recursion(input_length))
 end = time.time()
-print("Elapsed time",end - start)        
-        
- 
-       
+print('Elapsed time',end - start)
